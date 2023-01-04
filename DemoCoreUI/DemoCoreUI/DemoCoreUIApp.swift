df40cf4e14e836cd480b9f7fc863edc581933604
Akip Maulana <akip.maulana@gmail.com>
2023-01-04 19:17:43 +0700

    Create Demo and Package

---
+++ DemoCoreUI/DemoCoreUI/DemoCoreUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  DemoCoreUIApp.swift
+//  DemoCoreUI
+//
+//  Created by Akirah Dev on 04/01/23.
+//  
+//
+
+import SwiftUI
+
+@main
+struct DemoCoreUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
